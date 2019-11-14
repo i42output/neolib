b823cf2f1754027c13ec1ba6b5310d1859895ca0
Leigh Johnston <leigh@i42.co.uk>
2019-11-14 19:47:04 +0000

    Fixing g++ compilation errors.

--- include/neolib/pair.hpp
+++ include/neolib/pair.hpp
@@ -77,6 +77,6 @@ namespace neolib
         struct abstract_class_possible<std::pair<T1, T2>> { static constexpr bool value = false; };
 
         template<typename T1, typename T2>
-        struct abstract_type<std::pair<T1, T2>> { typedef typename abstract_type<T2>::type type; };
+        struct abstract_type<std::pair<T1, T2>, sfinae> { typedef typename abstract_type<T2>::type type; };
     }
 }
