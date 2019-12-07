ea2c29c97ffa85dd8a298a5431f425c5485485d9
Leigh Johnston <leigh@i42.co.uk>
2019-12-07 14:58:04 +0000

    Events rework (performance refactor)

--- include/neolib/event.hpp
+++ include/neolib/event.hpp
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <neolib/neolib.hpp>
 #include <vector>
+#include <deque>
 #include <optional>
 #include <mutex>
 #include <atomic>
