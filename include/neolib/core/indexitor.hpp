2849d91d6e85580472fcc56024206a08eeb55c97
Leigh Johnston <leigh@i42.co.uk>
2020-11-30 23:46:08 +0000

    C++17 -> C++20

--- include/neolib/core/indexitor.hpp
+++ include/neolib/core/indexitor.hpp
@@ -52,11 +52,11 @@ namespace neolib
         typedef T data_type;
         typedef ForeignIndex foreign_index_type;
         typedef std::pair<data_type, const foreign_index_type> value_type;
+        typedef value_type& reference;
+        typedef const value_type& const_reference;
         typedef std::pair<const foreign_index_type, const foreign_index_type> skip_type;
         typedef Alloc allocator_type;
-        typedef typename std::allocator_traits<allocator_type>::reference reference;
         typedef typename std::allocator_traits<allocator_type>::pointer pointer;
-        typedef typename std::allocator_traits<allocator_type>::const_reference const_reference;
         typedef typename std::allocator_traits<allocator_type>::const_pointer const_pointer;
         typedef typename std::allocator_traits<allocator_type>::size_type size_type;
         typedef typename std::allocator_traits<allocator_type>::difference_type difference_type;
--- include/neolib/core/tag_array.hpp
+++ include/neolib/core/tag_array.hpp
@@ -50,10 +50,10 @@ namespace neolib
         typedef neolib::array_tree<Alloc> base_type;
     public:
         typedef T value_type;
+        typedef value_type& reference;
+        typedef const value_type& const_reference;
         typedef Alloc allocator_type;
-        typedef typename std::allocator_traits<allocator_type>::reference reference;
         typedef typename std::allocator_traits<allocator_type>::pointer pointer;
-        typedef typename std::allocator_traits<allocator_type>::const_reference const_reference;
         typedef typename std::allocator_traits<allocator_type>::const_pointer const_pointer;
         typedef typename std::allocator_traits<allocator_type>::size_type size_type;
         typedef typename std::allocator_traits<allocator_type>::difference_type difference_type;
