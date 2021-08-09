d7a2103b16956e3148810475555bf77428397ba6
Leigh Johnston <leigh@i42.co.uk>
2021-08-09 13:17:10 +0100

    Added failing tests (tree container)

--- unit_tests/Containers/Containers.cpp
+++ unit_tests/Containers/Containers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <neolib/core/optional.hpp>
-#include <neolib/core/tree.hpp>
 #include <neolib/core/jar.hpp>
 
 struct i_foo
@@ -18,7 +17,7 @@ struct foo : i_foo
 
 template class neolib::basic_jar<foo>;
 
-template class neolib::segmented_tree<std::string, 64, std::allocator<std::string>>;
+void TestTree();
 
 int main()
 {
@@ -51,66 +50,6 @@ int main()
 
     jar.item_cookie(jar.at_index(1));
 
-    neolib::tree<std::string> tree;
-    auto entities = tree.insert(tree.send(), "Entity");
-    auto components = tree.insert(tree.send(), "Component");
-    auto systems = tree.insert(tree.send(), "System");
-    auto animals = tree.insert(entities.end(), "Animals");
-    auto people = tree.insert(entities.end(), "People");
-    auto athletes = tree.insert(people.end(), "Athletes (London 2012 Gold Medalists, Running)");
-    tree.push_back(animals, "Dolphin");
-    tree.push_back(animals, "Kitten");
-    tree.push_back(animals, "Hedgehog");
-    tree.push_back(athletes, "Usain Bolt");
-    tree.push_back(athletes, "Usain Bolt");
-    tree.push_back(athletes, "Kirani James");
-    tree.push_back(athletes, "David Rudisha");
-    tree.push_back(athletes, "Taoufik Makhloufi");
-    tree.push_back(athletes, "Mo Farah");
-    tree.push_back(athletes, "Mo Farah");
-    tree.push_back(athletes, "Shelly-Ann Fraser-Pryce");
-    tree.push_back(athletes, "Allyson Felix");
-    tree.push_back(athletes, "Sanya Richards-Ross");
-    tree.push_back(athletes, "Caster Semenya");
-    tree.push_back(athletes, "Maryam Yusuf Jamal");
-    tree.push_back(athletes, "Meseret Defar Tola");
-    tree.push_back(athletes, "Tirunesh Dibaba Kenene");
-
-    std::cout << "Entire tree:-" << std::endl;
-    for (auto const& e : tree)
-        std::cout << e << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Entire tree (with depth):-" << std::endl;
-    for (auto i = tree.begin(); i != tree.end(); ++i)
-        std::cout << std::string(i.depth() * 4, ' ') << *i << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Entire tree (reverse iteration, with depth):-" << std::endl;
-    for (auto i = tree.rbegin(); i != tree.rend(); ++i)
-        std::cout << std::string(std::prev(i.base()).depth() * 4, ' ') << *i << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Entire tree (sorted, with descendent counts):-" << std::endl;
-    tree.sort();
-    for (auto i = tree.begin(); i != tree.end(); ++i)
-        std::cout << std::string(i.depth() * 4, ' ') << *i << " (" << i.descendent_count() << ")" << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Tree after leaf node erase:-" << std::endl;
-    tree.erase(components);
-    for (auto const& e : tree)
-        std::cout << e << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Tree after branch node erase:-" << std::endl;
-    tree.erase(people);
-    for (auto const& e : tree)
-        std::cout << e << std::endl;
-
-    /*
-    std::cout << std::endl;
-    std::cout << "Just Animals:-" << std::endl;
-    for (auto const& a : animals)
-        std::cout << a << std::endl; */
+    TestTree();
 }
+
---
+++ unit_tests/Containers/Tree.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <neolib/core/tree.hpp>
+
+template class neolib::segmented_tree<std::string, 64, std::allocator<std::string>>; 
+
+void TestTree()
+{
+    neolib::tree<std::string> tree;
+    auto entities = tree.insert(tree.send(), "Entity");
+    auto components = tree.insert(tree.send(), "Component");
+    auto systems = tree.insert(tree.send(), "System");
+    auto shapes = tree.insert(entities.end(), "Shapes");
+    auto animals = tree.insert(entities.end(), "Animals");
+    auto people = tree.insert(entities.end(), "People");
+    auto athletes = tree.insert(people.end(), "Athletes (London 2012 Gold Medalists, Running)");
+    tree.push_back(shapes, "Square");
+    tree.push_back(shapes, "Triangle");
+    tree.push_back(shapes, "Circle");
+    tree.push_back(animals, "Dolphin");
+    tree.push_back(animals, "Kitten");
+    tree.push_back(animals, "Hedgehog");
+    tree.push_back(athletes, "Usain Bolt");
+    tree.push_back(athletes, "Usain Bolt");
+    tree.push_back(athletes, "Kirani James");
+    tree.push_back(athletes, "David Rudisha");
+    tree.push_back(athletes, "Taoufik Makhloufi");
+    tree.push_back(athletes, "Mo Farah");
+    tree.push_back(athletes, "Mo Farah");
+    tree.push_back(athletes, "Shelly-Ann Fraser-Pryce");
+    tree.push_back(athletes, "Allyson Felix");
+    tree.push_back(athletes, "Sanya Richards-Ross");
+    tree.push_back(athletes, "Caster Semenya");
+    tree.push_back(athletes, "Maryam Yusuf Jamal");
+    tree.push_back(athletes, "Meseret Defar Tola");
+    tree.push_back(athletes, "Tirunesh Dibaba Kenene");
+
+    std::cout << "Entire tree:-" << std::endl;
+    for (auto const& e : tree)
+        std::cout << e << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Entire tree (with depth):-" << std::endl;
+    for (auto i = tree.begin(); i != tree.end(); ++i)
+        std::cout << std::string(i.depth() * 4, ' ') << *i << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Entire tree (reverse iteration, with depth):-" << std::endl;
+    for (auto i = tree.rbegin(); i != tree.rend(); ++i)
+        std::cout << std::string(std::prev(i.base()).depth() * 4, ' ') << *i << std::endl;
+
+    tree.sort();
+    std::cout << std::endl;
+    std::cout << "Entire tree (sorted, with descendent counts):-" << std::endl;
+    for (auto i = tree.begin(); i != tree.end(); ++i)
+        std::cout << std::string(i.depth() * 4, ' ') << *i << " (" << i.descendent_count() << ")" << std::endl;
+
+    // note: saved iterators are invalidated after the sort.
+
+    components = std::find(tree.sbegin(), tree.send(), "Component");
+    tree.erase(components);
+    std::cout << std::endl;
+    std::cout << "Tree after leaf node erase:-" << std::endl;
+    for (auto i = tree.begin(); i != tree.end(); ++i)
+        std::cout << std::string(i.depth() * 4, ' ') << *i << " (" << i.descendent_count() << ")" << std::endl;
+
+    shapes = std::find(tree.sbegin(), tree.send(), "Shapes");
+    tree.erase(shapes);
+    std::cout << std::endl;
+    std::cout << "Tree after branch node erase:-" << std::endl;
+    for (auto i = tree.begin(); i != tree.end(); ++i)
+        std::cout << std::string(i.depth() * 4, ' ') << *i << " (" << i.descendent_count() << ")" << std::endl;
+
+    /*
+    std::cout << std::endl;
+    std::cout << "Just Animals:-" << std::endl;
+    for (auto const& a : animals)
+        std::cout << a << std::endl; */
+}
