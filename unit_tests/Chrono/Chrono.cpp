a51c5b82c36e04a4fecefa2de6577b08aabf3fbd
Leigh Johnston <leigh@i42.co.uk>
2026-06-15 23:09:24 +0100

    tsc_clock -- Claude refactor

--- unit_tests/Chrono/Chrono.cpp
+++ unit_tests/Chrono/Chrono.cpp
@@ -7,7 +7,7 @@ int main()
     std::chrono::high_resolution_clock::now();
     neolib::chrono::fast_clock::now();
 
-    std::uint64_t n = 0;
+    std::uint64_t defeatOptimiser = 0;
 
 #ifdef NDEBUG
     int constexpr iterations = 100000000;
@@ -17,20 +17,19 @@ int main()
 
     auto scStart = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; ++i)
-        n += std::chrono::steady_clock::now().time_since_epoch().count();
+        defeatOptimiser += std::chrono::steady_clock::now().time_since_epoch().count();
     auto scEnd = std::chrono::high_resolution_clock::now();
 
     auto hrcStart = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; ++i)
-        n += std::chrono::high_resolution_clock::now().time_since_epoch().count();
+        defeatOptimiser += std::chrono::high_resolution_clock::now().time_since_epoch().count();
     auto hrcEnd = std::chrono::high_resolution_clock::now();
 
     auto fcStart = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; ++i)
-        n += neolib::chrono::fast_clock::now().time_since_epoch().count();;
+        defeatOptimiser += neolib::chrono::fast_clock::now().time_since_epoch().count();
     auto fcEnd = std::chrono::high_resolution_clock::now();
 
-    std::cout << n << std::endl;
     std::cout << "steady_clock(1): " << std::chrono::duration_cast<std::chrono::microseconds>(scEnd - scStart).count() << std::endl;
     std::cout << "high_resolution_clock(1): " << std::chrono::duration_cast<std::chrono::microseconds>(hrcEnd - hrcStart).count() << std::endl;
     std::cout << "fast_clock(1): " << std::chrono::duration_cast<std::chrono::microseconds>(fcEnd - fcStart).count() << std::endl;
@@ -41,4 +40,14 @@ int main()
 
     std::cout << "high_resolution_clock(2): " << std::chrono::duration_cast<std::chrono::milliseconds>(t1.first - t0.first) << std::endl;
     std::cout << "fast_clock(2): " << std::chrono::duration_cast<std::chrono::milliseconds>(t1.second - t0.second) << std::endl;
+
+    auto t2 = std::make_pair(std::chrono::high_resolution_clock::now(), neolib::chrono::fast_clock::now());
+    for (int i = 0; i < 100000; ++i)
+        defeatOptimiser += std::rand();
+    auto t3 = std::make_pair(std::chrono::high_resolution_clock::now(), neolib::chrono::fast_clock::now());
+
+    std::cout << "high_resolution_clock(3): " << std::chrono::duration_cast<std::chrono::microseconds>(t3.first - t2.first) << std::endl;
+    std::cout << "fast_clock(3): " << std::chrono::duration_cast<std::chrono::microseconds>(t3.second - t2.second) << std::endl;
+
+    std::cout << defeatOptimiser << std::endl;
 }
