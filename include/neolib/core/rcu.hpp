2c8274ac25e4efe0193a4058725941ebff9c0559
Leigh Johnston <leigh@i42.co.uk>
2026-08-30 22:06:51 +0100

    rcu_update

---
+++ include/neolib/core/rcu.hpp
@@ -0,0 +1,62 @@
+// rcu.hpp
+/*
+ *  Copyright (c) 2026 Leigh Johnston.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of Leigh Johnston nor the names of any
+ *       other contributors to this software may be used to endorse or
+ *       promote products derived from this software without specific prior
+ *       written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <neolib/neolib.hpp>
+
+#include <atomic>
+#include <memory>
+
+namespace neolib
+{
+    template <typename T, typename Fn>
+    inline void rcu_update(std::atomic<std::shared_ptr<T const>>& aTarget, Fn&& aFn)
+    {
+        auto expected = aTarget.load(std::memory_order_acquire);
+        std::shared_ptr<T> scratch;
+        for (;;)
+        {
+            if (scratch != nullptr && scratch.use_count() == 1)
+                *scratch = *expected;
+            else
+                scratch = std::make_shared<T>(*expected);
+            aFn(*scratch);
+            std::shared_ptr<T const> desired = scratch;
+            if (aTarget.compare_exchange_weak(expected, desired, std::memory_order_acq_rel, std::memory_order_acquire))
+                return;
+        }
+    }
+}
